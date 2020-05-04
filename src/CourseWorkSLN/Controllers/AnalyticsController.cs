3cec6a9de4dfb956d92bdb0d95f3933b15df509c
Dmitry-Gerasimenko <57838564+Dmitry-Gerasimenko@users.noreply.github.com>
2020-05-05 01:08:28 +0300

    BL pre-release

--- src/CourseWorkSLN/Controllers/AnalyticsController.cs
+++ src/CourseWorkSLN/Controllers/AnalyticsController.cs
@@ -85,6 +85,86 @@ namespace CourseWorkSLN.Controllers
             }
         }
 
+        [HttpPost]
+        public async Task<ActionResult> GetTimeChart(SurfaceInfoViewModel model)
+        {
+            string outFileName = $"{DateTime.Now.ToString("yymmssfff")}-performance-testing.txt";
+
+            var timeArr = new List<long>();
+            var threadsArr = new [] { 1, 2, 3, 5, 10, 15, 30 };
+
+            foreach (var thrdCount in threadsArr)
+            {
+                var tasks = new List<Task<double>>();
+                double hX = (model.XEnd - model.XStart) / thrdCount;
+                double hY = (model.YEnd - model.YStart) / thrdCount;
+
+                for (int i = 0; i < thrdCount; i++)
+                {
+                    double xnStep = model.XStart + hX * i;
+                    double xkStep = model.XStart + hX * (i + 1);
+
+                    tasks.Add(new Task<double>(() =>
+                        SurfaceAreaService.CalculateSurfaceArea(
+                            xnStep, xkStep,
+                            model.YStart, model.YEnd,
+                            model.Steps / thrdCount,
+                            outFileName)));
+                }
+
+                var startTime = Stopwatch.StartNew();
+
+                tasks.ForEach(t => t.Start());
+                Task.WaitAll(tasks.ToArray());
+                var res = tasks.Sum(t => t.Result);
+
+                startTime.Stop();
+
+                timeArr.Add(startTime.ElapsedMilliseconds);
+            }
+
+            return new ObjectResult( new { threadsArr = threadsArr, timeArr = timeArr.ToArray() });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> GetAccuracyChart(SurfaceInfoViewModel model)
+        {
+            var exactAreaResult = await GetExactArea(model);
+            string outFileName = $"{DateTime.Now.ToString("yymmssfff")}-accuracy-testing.txt";
+
+            var accuracyArr = new List<double>();
+            var threadsArr = new[] { 1, 2, 3, 5, 10, 15, 30 };
+
+            foreach (var thrdCount in threadsArr)
+            {
+                var tasks = new List<Task<double>>();
+                double hX = (model.XEnd - model.XStart) / thrdCount;
+                double hY = (model.YEnd - model.YStart) / thrdCount;
+
+                for (int i = 0; i < thrdCount; i++)
+                {
+                    double xnStep = model.XStart + hX * i;
+                    double xkStep = model.XStart + hX * (i + 1);
+
+                    tasks.Add(new Task<double>(() =>
+                        SurfaceAreaService.CalculateSurfaceArea(
+                            xnStep, xkStep,
+                            model.YStart, model.YEnd,
+                            model.Steps / thrdCount,
+                            outFileName)));
+                }
+                tasks.ForEach(t => t.Start());
+                Task.WaitAll(tasks.ToArray());
+
+                var res = tasks.Sum(t => t.Result);
+            
+
+                accuracyArr.Add(Math.Abs(res - exactAreaResult));
+            }
+
+            return new ObjectResult(new { threadsArr, accuracyArr = accuracyArr.ToArray() });
+        }
+
         private async Task<double> GetExactArea(SurfaceInfoViewModel model)
         {
             HttpClient client = new HttpClient();
