de190589caa735ab8917ebfbd349ac6a7cf5d2e1
Dmitry-Gerasimenko <57838564+Dmitry-Gerasimenko@users.noreply.github.com>
2020-05-04 21:22:44 +0300

    Before the full refactoring

---
+++ src/CourseWorkSLN/Controllers/AnalyticsController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BusinessLogicLayer.Services;
+using CourseWorkSLN.Models;
+using CourseWorkSLN.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseWorkSLN.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        private const double MaxStep = 2.002;
+
+        public IActionResult Index()
+        {
+            return View(new SurfaceInfoViewModel
+            {
+                Name = "Pow(x, 2) + Pow(y, 2) + Exp(x) + Exp(y)",
+                XStart = -10,
+                XEnd = 10,
+                YStart = -10,
+                YEnd = 10,
+                Step = 2,
+                IsParallel = true,
+                ThreadsCount = 1,
+            });
+        }
+
+        public async Task<IActionResult> GetExactData(SurfaceInfoViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Заполните обязательные поля корректно.");
+            }
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5000");
+
+            var formContent = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("xstart", Convert.ToString(model.XStart)),
+                new KeyValuePair<string, string>("xend", Convert.ToString(model.XEnd)),
+                new KeyValuePair<string, string>("ystart", Convert.ToString(model.YStart)),
+                new KeyValuePair<string, string>("yend", Convert.ToString(model.YEnd)),
+                new KeyValuePair<string, string>("step", Convert.ToString(2.5 - model.Step)),
+            });
+
+            var response = await client.PostAsync("get_exact_data", formContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return Content(await response.Content.ReadAsStringAsync());
+            }
+
+            return BadRequest(response.ReasonPhrase);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> SolveTaskArea(SurfaceInfoViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Заполните обязательные поля корректно.");
+            }
+
+            try
+            {
+                if (model.IsParallel)
+                {
+                    var viewResult = GetAreaWithParallel(model);
+
+                    return View("_AreaPartial", viewResult);
+                }
+                else
+                {
+                    var viewResult = GetAreaWithoutParallel(model);
+
+                    return View("_AreaPartial", viewResult);
+                }
+            }
+            catch (Exception ex)
+            {
+                HttpContext.Response.StatusCode = 500;
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return View("_AreaErrorPartial", ex.Message);
+            }
+        }
+
+        private AreaResultViewModel GetAreaWithoutParallel(SurfaceInfoViewModel model)
+        {
+            string outFileName = $"{DateTime.Now.ToString("yymmssfff")}-thread-calculations.txt";
+
+            var startTime = Stopwatch.StartNew();
+            double res = SurfaceAreaService.CalculateSurfaceArea(model.XStart, model.XEnd,
+                                                                 model.YStart, model.YEnd,
+                                                                 MaxStep - model.Step,
+                                                                 MaxStep - model.Step,
+                                                                 outFileName);
+
+            startTime.Stop();
+
+            var resultTime = startTime.Elapsed;
+            return new AreaResultViewModel { Result = res, Description = resultTime.ToString(), };
+        }
+
+        private AreaResultViewModel GetAreaWithParallel(SurfaceInfoViewModel model)
+        {
+            string outFileName = $"{DateTime.Now.ToString("yymmssfff")}-thread-calculations.txt";
+
+            var tasks = new List<Task<double>>();
+            double hX = (model.XEnd - model.XStart) / model.ThreadsCount;
+            double hY = (model.YEnd - model.YStart) / model.ThreadsCount;
+
+            for (int i = 0; i < model.ThreadsCount; i++)
+            {
+                double xnStep = model.XStart + hX * i;
+                double xkStep = model.XStart + hX * (i + 1);
+
+                tasks.Add(new Task<double>(() =>
+                    SurfaceAreaService.CalculateSurfaceArea(
+                        xnStep, xkStep,
+                        model.YStart, model.YEnd,
+                        MaxStep - model.Step,
+                        MaxStep - model.Step,
+                        outFileName)));
+            }
+
+            var startTime = Stopwatch.StartNew();
+
+            tasks.ForEach(t => t.Start());
+            Task.WaitAll(tasks.ToArray());
+
+            var res = tasks.Sum(t => t.Result);
+
+            startTime.Stop();
+
+            var resultTime = startTime.Elapsed;
+            return new AreaResultViewModel { Result = res, Description = resultTime.ToString(), };
+        }
+    }
+}
--- src/SurfacePointAPI_python/SurfacePointAPI_python.py
+++ src/SurfacePointAPI_python/SurfacePointAPI_python.py
@@ -2,8 +2,11 @@ import numpy as np
 import numexpr as ne
 from flask import Flask, render_template, json, request
 from flask_cors import CORS
-
 import json 
+from scipy import integrate
+
+import pylab;
+from mpl_toolkits.mplot3d import Axes3D
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -13,6 +16,8 @@ class NumpyEncoder(json.JSONEncoder):
 
 app = Flask(__name__)
 CORS(app);
+
+
 
 @app.route('/init_data', methods=['GET'])
 def init_data():
@@ -41,6 +46,40 @@ def get_data():
     zgrid = ne.evaluate(name)
 
     return  json.dumps(zgrid, cls=NumpyEncoder)
+
+@app.route('/get_exact_data', methods=['GET', 'POST'])
+def get_exact_data():
+
+    def f_diff_x(x):
+        return 2*x + np.exp(x)
+
+    def f_diff_y(y):
+        return 2*y + np.exp(y)
+
+    f = lambda x, y: np.sqrt(1 + f_diff_x(x)**2 + f_diff_y(y)**2)
+
+    result = integrate.dblquad(f, -10, 10, lambda x: -10, lambda x: 10)[0]
+
+    xstart = request.form['xstart'];
+    xend = request.form['xend'];
+    ystart = request.form['ystart'];
+    yend = request.form['yend'];
+    step = request.form['step'];
+
+    xRange = np.arange(float(xstart), float(xend), float(step));
+    yRange = np.arange(float(ystart), float(yend), float(step));
+    
+    x, y = np.meshgrid(xRange, yRange)
+    zgrid = x ** 2 + y ** 2 + np.exp(x) + np.exp(y)
+
+    fig = pylab.figure()
+    axes = Axes3D(fig)
+    axes.plot_surface(x, y, zgrid)
+    
+    pylab.show()
+
+
+    return  json.dumps(result)
  
 
 
