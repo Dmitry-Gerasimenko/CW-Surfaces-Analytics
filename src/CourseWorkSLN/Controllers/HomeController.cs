694c57cc76faf16daa56d3dc543ad29eb561cbb6
Dmitry-Gerasimenko <57838564+Dmitry-Gerasimenko@users.noreply.github.com>
2020-05-05 02:01:38 +0300

    Ladning page added

--- src/CourseWorkSLN/Controllers/HomeController.cs
+++ src/CourseWorkSLN/Controllers/HomeController.cs
@@ -122,6 +122,11 @@ namespace CourseWorkSLN.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         private AreaResultViewModel GetAreaWithoutParallel(SurfaceInfoViewModel model)
         {
             var startTime = Stopwatch.StartNew();
